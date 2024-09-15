fa818f9e616db51ed9c05547c8076e7799c445e5
attamakevin <kevinejikez@gmail.com>
2024-09-15 22:15:54 +0100

    fixed loin

--- app/auth/views.py
+++ app/auth/views.py
@@ -60,17 +60,25 @@ def login():
     data = request.get_json()
     username = data.get('username')
     password = data.get('password')
-    
+
     user = User.query.filter_by(username=username).first()
-    
+
     if user is None or not user.check_password(password):
         return jsonify({"error": "Invalid credentials"}), 401
-    
+
     access_token = create_access_token(identity={'username': user.username}, expires_delta=timedelta(hours=1))
     refresh_token = create_refresh_token(identity={'username': user.username})
-    print (access_token)
-    print ("access_token")
-    return jsonify({"access_token": access_token, "refresh_token": refresh_token}), 200
+
+    # Return user data along with the tokens
+    return jsonify({
+        "access_token": access_token,
+        "refresh_token": refresh_token,
+        "user": {
+            "username": user.username,
+            "email": user.email,
+            "is_admin": user.is_admin,
+        }
+    }), 200
 
 #@auth_blueprint.route('/logout', methods=['POST'])
 #@jwt_required()
