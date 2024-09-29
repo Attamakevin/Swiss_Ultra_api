41c863c6e3fce81a0d387b512e9430883c527efe
attamakevin <kevinejikez@gmail.com>
2024-09-29 02:01:25 +0100

    fixed loin

--- app/models.py
+++ app/models.py
@@ -23,6 +23,12 @@ class TransactionLog(db.Model):
     amount = db.Column(db.Float, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     description = db.Column(db.String(250), nullable=True)  # Optional description for transaction
+    
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
 
 class Transfer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -42,11 +48,6 @@ class Transfer(db.Model):
     # Define relationship with Notification model
     notifications = db.relationship('Notification', backref='user', lazy=True)
 
-    def set_password(self, password):
-        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
-
-    def check_password(self, password):
-        return bcrypt.check_password_hash(self.password_hash, password)
 
     def add_notification(self, message):
         notification = Notification(message=message, user_id=self.id)
